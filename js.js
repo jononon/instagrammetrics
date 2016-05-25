4468102aeca8437309aaacef286afaa2180c6bae
Jonathan Damico <jj_damico@yahoo.com>
2016-05-25 16:43:13 -0700

    init commit - test metric

---
+++ js.js
@@ -0,0 +1,18 @@
+var accessToken = "235187851.1677ed0.d72283c5f7f24e51acc3833c7929e3b1";
+
+$(document).ready(function() {
+  $.ajax({
+    url: 'https://api.instagram.com/v1/users/self/media/recent?access_token=' + accessToken,
+    dataType: 'json',
+    success: function(data) {
+      var sumLikes, numPictures;
+      for(i = 0; i<data.data.length; i++) {
+        numPictures++;
+        sumLikes+=data.data[i].likes.count;
+      }
+      $('#metric').append(sumLikes/numPictures);
+    },
+    async: false,
+    timeout: 5000,
+  });
+});

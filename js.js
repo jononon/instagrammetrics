2d48c404d4c61cee6dad9101c1d826ae6f9983b6
Jonathan Damico <jj_damico@yahoo.com>
2016-05-25 17:35:09 -0700

    refine metric to mean bigger is better

--- js.js
+++ js.js
@@ -1,4 +1,7 @@
 var accessToken = "235187851.1677ed0.d72283c5f7f24e51acc3833c7929e3b1";
+var sumLikes = 0;
+var numPictures = 0;
+var followers = 0;
 
 $(document).ready(function() {
   $.ajax({
@@ -6,14 +9,28 @@ $(document).ready(function() {
     dataType: 'jsonp',
     crossDomain: true,
     success: function(data) {
-      var sumLikes = 0, numPictures = 0;
       for(i = 0; i<data.data.length; i++) {
         numPictures++;
         sumLikes+=data.data[i].likes.count;
       }
-      $('#metric').append(sumLikes/numPictures);
     },
     async: false,
     timeout: 5000,
   });
+  $.ajax({
+    url: 'https://api.instagram.com/v1/users/self/?access_token=' + accessToken,
+    dataType: 'jsonp',
+    crossDomain: true,
+    success: function(data) {
+      followers = data.counts.followed_by;
+    },
+    async: false,
+    timeout: 5000,
+  });
+
+  $('#metric').append(((sumLikes/numPictures)/followers)*100);
 });
+
+function update(username) {
+
+}
